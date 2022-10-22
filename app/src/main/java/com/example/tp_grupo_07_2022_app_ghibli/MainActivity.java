8b1c9157e735f424f83bbf0b6ad9f74536ec3b25
lady-coder-16 <ayortiz16@gmail.com>
2022-10-21 21:43:38 -0300

    Se agrega dashboard design al activity main

--- app/src/main/java/com/example/tp_grupo_07_2022_app_ghibli/MainActivity.java
+++ app/src/main/java/com/example/tp_grupo_07_2022_app_ghibli/MainActivity.java
@@ -7,17 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etnota1, etnota2;
-    Button btnCalcular;
-    TextView tvResultado;
     Toolbar menu_toolbar;
 
     @Override
@@ -31,24 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
         saludarUsuario();
         //hacemos la conexión de la variable local con la variable del archivo activity_main
-        etnota1 = findViewById(R.id.etnota1);
-        etnota2 = findViewById(R.id.etnota2);
-        tvResultado = findViewById(R.id.tvResultado);
-
-        btnCalcular = findViewById(R.id.btnCalcular);
-
-        btnCalcular.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Toast.makeText(MainActivity.this, "Se apreto el botón de calcular", Toast.LENGTH_SHORT).show();
-
-                int nota1 = Integer.valueOf(etnota1.getText().toString());
-                int nota2 = Integer.valueOf(etnota2.getText().toString());
-
-                calcular(nota1, nota2);
-            }
-        });
-
 
     }
 
@@ -77,8 +52,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void calcular(int nota1, int nota2) {
-        int promedio = (nota1 + nota2) / 2;
-        tvResultado.setText("Resultado: " + promedio);
-    }
 }
