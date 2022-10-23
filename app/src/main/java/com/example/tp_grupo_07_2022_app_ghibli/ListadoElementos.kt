a6fc16ce3fa6c428840de352b23fefc8ed03b0a2
lady-coder-16 <ayortiz16@gmail.com>
2022-10-23 11:01:06 -0300

    Se agrega id a los cardviews de activity main y se hace las conexiones a la nueva activity creada en kotlin

---
+++ app/src/main/java/com/example/tp_grupo_07_2022_app_ghibli/ListadoElementos.kt
@@ -0,0 +1,11 @@
+package com.example.tp_grupo_07_2022_app_ghibli
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListadoElementos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listado_elementos)
+    }
+}
--- app/src/main/java/com/example/tp_grupo_07_2022_app_ghibli/MainActivity.java
+++ app/src/main/java/com/example/tp_grupo_07_2022_app_ghibli/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.tp_grupo_07_2022_app_ghibli;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Toolbar menu_toolbar;
+
+    CardView cardPeliculas, cardPersonajes, cardLugares, cardEspecies, cardVehiculos, cardInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +53,56 @@ public class MainActivity extends AppCompatActivity {
             String usuario = bundle.getString("usuario");
 
             Toast.makeText(MainActivity.this, "Bienvenido " + usuario, Toast.LENGTH_SHORT).show();
+
+            cardPeliculas = (CardView) findViewById(R.id.cvPeliculas);
+            cardPersonajes = (CardView) findViewById(R.id.cvPersonajes);
+            cardLugares = (CardView) findViewById(R.id.cvLugares);
+            cardEspecies = (CardView) findViewById(R.id.cvEspecies);
+            cardVehiculos = (CardView) findViewById(R.id.cvVehiculos);
+            cardInfo = (CardView) findViewById(R.id.cvInfo);
+
+            cardPeliculas.setOnClickListener(this);
+            cardPersonajes.setOnClickListener(this);
+            cardLugares.setOnClickListener(this);
+            cardEspecies.setOnClickListener(this);
+            cardVehiculos.setOnClickListener(this);
+            cardInfo.setOnClickListener(this);
         }
     }
 
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        switch (view.getId()){
+            case R.id.cvPeliculas:
+                i= new Intent(this,ListadoElementos.class);
+                startActivity(i);
+                break;
+
+            case R.id.cvPersonajes:
+                i= new Intent(this,ListadoElementos.class);
+                startActivity(i);
+                break;
+
+            case R.id.cvLugares:
+                i= new Intent(this,ListadoElementos.class);
+                startActivity(i);
+                break;
+
+            case R.id.cvEspecies:
+                i= new Intent(this,ListadoElementos.class);
+                startActivity(i);
+                break;
+
+            case R.id.cvVehiculos:
+                i= new Intent(this,ListadoElementos.class);
+                startActivity(i);
+                break;
+
+            case R.id.cvInfo:
+                i= new Intent(this,ListadoElementos.class);
+                startActivity(i);
+                break;
+        }
+    }
 }
