f258d512b0f949a20e91949b1b297bbaa86caf89
lady-coder-16 <ayortiz16@gmail.com>
2022-11-13 13:15:39 -0300

    Se agregan Toolbar y Menu con nuevos iconos, y se actualizan algunas dependencias

--- app/src/main/java/com/example/tp_grupo_07_2022_app_ghibli/MainActivity.java
+++ app/src/main/java/com/example/tp_grupo_07_2022_app_ghibli/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tp_grupo_07_2022_app_ghibli;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
@@ -7,43 +8,83 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Toolbar menu_toolbar;
 
-    CardView cardPeliculas, cardPersonajes, cardLugares, cardEspecies, cardVehiculos, cardInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        menu_toolbar = findViewById(R.id.menu_toolbar);
-        setSupportActionBar(menu_toolbar);
+        Toolbar toolbar;
+        CardView cardPeliculas, cardPersonajes, cardLugares, cardEspecies, cardVehiculos, cardInfo;
+
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar); //debe ir antes que los getSupport
         getSupportActionBar().setTitle("Menú principal");
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
 
         saludarUsuario();
         //hacemos la conexión de la variable local con la variable del archivo activity_main
 
+
+        cardPeliculas = (CardView) findViewById(R.id.cvPeliculas);
+        cardPersonajes = (CardView) findViewById(R.id.cvPersonajes);
+        cardLugares = (CardView) findViewById(R.id.cvLugares);
+        cardEspecies = (CardView) findViewById(R.id.cvEspecies);
+        cardVehiculos = (CardView) findViewById(R.id.cvVehiculos);
+        cardInfo = (CardView) findViewById(R.id.cvInfo);
+
+        cardPeliculas.setOnClickListener(this);
+        cardPersonajes.setOnClickListener(this);
+        cardLugares.setOnClickListener(this);
+        cardEspecies.setOnClickListener(this);
+        cardVehiculos.setOnClickListener(this);
+        cardInfo.setOnClickListener(this);
     }
 
-    public boolean onCreateOptionMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main,menu);
         return super.onCreateOptionsMenu(menu);
     }
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        if(item.getItemId() == R.id.item_agregar){
-            //realizar accion
-            Intent intent = new Intent(MainActivity.this, AgregarActivity.class);
-            startActivity(intent);
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+
+            case R.id.buscar:
+                Toast.makeText(this,"Buscar",Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.filtro:
+                Toast.makeText(this,"Filtro",Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_settings:
+                Toast.makeText(this,"Ajustes",Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.about_us:
+                Toast.makeText(this,"Sobre Nosotras",Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.salir:
+                Toast.makeText(this,"Cerrando Sesión...",Toast.LENGTH_SHORT).show();
+                return true;
+
+
+            default: return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
     }
 
     private void saludarUsuario() {
@@ -54,19 +95,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             Toast.makeText(MainActivity.this, "Bienvenido " + usuario, Toast.LENGTH_SHORT).show();
 
-            cardPeliculas = (CardView) findViewById(R.id.cvPeliculas);
-            cardPersonajes = (CardView) findViewById(R.id.cvPersonajes);
-            cardLugares = (CardView) findViewById(R.id.cvLugares);
-            cardEspecies = (CardView) findViewById(R.id.cvEspecies);
-            cardVehiculos = (CardView) findViewById(R.id.cvVehiculos);
-            cardInfo = (CardView) findViewById(R.id.cvInfo);
 
-            cardPeliculas.setOnClickListener(this);
-            cardPersonajes.setOnClickListener(this);
-            cardLugares.setOnClickListener(this);
-            cardEspecies.setOnClickListener(this);
-            cardVehiculos.setOnClickListener(this);
-            cardInfo.setOnClickListener(this);
         }
     }
 
